c61410cd1e3de71eb049eaa41daa704fc3205253
KieTuaNguyen <kietntgdd210002@fpt.edu.vn>
2023-02-21 22:40:51 +0700

    Completion-Assignment1-OOP

--- Presentator.cs
+++ Presentator.cs
@@ -5,22 +5,66 @@ namespace CSharp_Assignment1
     //Method
     public void DisplayMenu()
     {
+      Console.WriteLine("=============================================");
+      Console.WriteLine("|                                    v14.1.0|");
       Console.WriteLine("=========GREENWICH MANAGEMENT SYSTEM=========");
-      Console.WriteLine("1 | Add new teacher's information           |");
+      Console.WriteLine("1 | Add new course                          |");
       Console.WriteLine("2 | Add new student's information           |");
-      Console.WriteLine("3 | Display teacher information             |");
-      Console.WriteLine("4 | Display student information             |");
-      Console.WriteLine("5 | Exit the program                        |");
+      Console.WriteLine("3 | Display specific course information     |");
+      Console.WriteLine("4 | Display specific student information    |");
+      Console.WriteLine("5 | Show all courses in the system          |");
+      Console.WriteLine("6 | Show all students in the system         |");
+      Console.WriteLine("7 | Update specific student by their ID     |");
+      Console.WriteLine("8 | Delete specific student by their ID     |");
+      Console.WriteLine("9 | Exit the program                        |");
       Console.WriteLine("=============================================");
       Console.WriteLine("        Please choose your option:           ");
     }
 
-    public void DeleteInformation()
+    public void DisplaySuccessfulMessage()
     {
+      Console.WriteLine("=============================================");
+      Console.WriteLine("   The information is added successfully");
+      Console.WriteLine("=============================================");
     }
 
-    public void UpdateInformaton()
+    public void DisplayUpdateSuccessfulMessage()
     {
+      Console.WriteLine("=============================================");
+      Console.WriteLine("  The information is updated successfully");
+      Console.WriteLine("=============================================");
+    }
+
+    public void DisplayDeleteSuccessfulMessage()
+    {
+      Console.WriteLine("=============================================");
+      Console.WriteLine("  The information is deleted successfully");
+      Console.WriteLine("=============================================");
+    }
+
+    public void DisplayStudentNotExistMessage()
+    {
+      Console.WriteLine("==============STUDENT INFORMATION============");
+      Console.WriteLine("          The student is not existed      ");
+      Console.WriteLine("=============================================");
+    }
+
+    public void DisplayCourseNotExistMessage()
+    {
+      Console.WriteLine("==============COURSE INFORMATION=============");
+      Console.WriteLine("          The course is not existed      ");
+      Console.WriteLine("=============================================");
+    }
+
+    public void Exit()
+    {
+      Console.WriteLine("=============================================");
+      Console.WriteLine("      Thank you for using our system");
+      Console.WriteLine("=============================================");
+      Console.WriteLine("|         School Management System          |");
+      Console.WriteLine("|==============by KietGDD210002=============|");
+
+
     }
   }
 }
--- Program.cs
+++ Program.cs
@@ -2,18 +2,20 @@
 {
   class Program
   {
+    public static List<Student> students { get; internal set; }
+    public static List<Course> courses { get; internal set; }
+
     static void Main(string[] args)
     {
-      //Create list of teacher and student
-      List<Teacher> teachers = new List<Teacher>();
-      List<Student> students = new List<Student>();
-
-      //Create presentator object
+      //Create list of students and courses
+      students = new List<Student>();
+      courses = new List<Course>();
+      //Create object
+      Course course = new Course();
+      Student student = new Student();
       Presentator presentator = new Presentator();
 
-      //Menu
       presentator.DisplayMenu();
-
       Console.WriteLine("Enter your choice: ");
       int choice;
       do
@@ -22,40 +24,104 @@
         switch (choice)
         {
           case 1:
-            Teacher teacher = new Teacher();
-            teacher.InputInformation();
-            teachers.Add(teacher);
+            //Add new course
+            course = new Course();
+            course.InputInformation();
+            courses.Add(course);
+            presentator.DisplaySuccessfulMessage();
             presentator.DisplayMenu();
             break;
           case 2:
-            Student student = new Student();
-            student.InputInformation();
+            //Add new student's
+            student = new Student();
+            student.InputInformation(courses);
             students.Add(student);
+            presentator.DisplaySuccessfulMessage();
             presentator.DisplayMenu();
             break;
           case 3:
-            foreach (Teacher t in teachers)
+            //Display specific course information
+            Console.WriteLine("Enter course's name: ");
+            string courseName = Console.ReadLine();
+            bool found = false;
+            foreach (Course c in courses)
             {
-              t.DisplayInformation();
+              if (c.CourseName == courseName)
+              {
+                c.DisplayInformation();
+                found = true;
+                break;
+              }
+            }
+            if (!found)
+            {
+              presentator.DisplayCourseNotExistMessage();
+
             }
             presentator.DisplayMenu();
             break;
           case 4:
+            //Display specific student information
+            Console.WriteLine("Enter student's ID: ");
+            string studentID = Console.ReadLine();
+            bool foundStudent = false;
+            foreach (Student s in students)
+            {
+              if (s.StudentID == studentID)
+              {
+                s.DisplayInformation();
+                foundStudent = true;
+                break;
+              }
+            }
+            if (!foundStudent)
+            {
+              presentator.DisplayStudentNotExistMessage();
+            }
+            presentator.DisplayMenu();
+            break;
+          case 5:
+            HashSet<string> courseNames = new HashSet<string>();
+            foreach (Course c in courses)
+            {
+              if (!courseNames.Contains(c.CourseName))
+              {
+                courseNames.Add(c.CourseName);
+                c.DisplayInformation();
+              }
+            }
+            presentator.DisplayMenu();
+            break;
+          case 6:
             foreach (Student s in students)
             {
               s.DisplayInformation();
             }
             presentator.DisplayMenu();
             break;
-          case 5:
-            Console.WriteLine("Exit the program");
+          case 7:
+            student = new Student();
+            Console.WriteLine("Enter student's ID: ");
+            string studentIDUpdate = Console.ReadLine();
+            student.UpdateInformation(studentIDUpdate);
+            presentator.DisplayMenu();
+            break;
+          case 8:
+            student = new Student();
+            Console.WriteLine("Enter student's ID: ");
+            string studentIDDelete = Console.ReadLine();
+            student.DeleteInformation(studentIDDelete);
+            presentator.DisplayMenu();
+            break;
+          case 9:
+            presentator.Exit();
             break;
           default:
             Console.WriteLine("Invalid choice");
             break;
         }
       }
-      while (choice != 5);
+      while (choice != 9);
     }
   }
 }
